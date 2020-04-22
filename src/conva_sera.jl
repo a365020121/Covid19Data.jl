a2d01b25ff0e0174d0b96c52800e60276dff1213
Yishan Peng <787137537@qq.com>
2020-04-22 12:42:28 +1000

    Update conva_sera.jl

--- src/conva_sera.jl
+++ src/conva_sera.jl
@@ -4,6 +4,8 @@ import HTTP
 import CSV
 import DataFrames
 
+using DataFrames
+
 
 
 confirmedFile =  "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
